77b85334d75f606d91315cb59b86def992a67b0e
atsawinhero <atsawin.hero@gmail.com>
2026-03-05 17:49:59 +0700

    Update super_sensor.js

--- super_sensor.js
+++ super_sensor.js
@@ -1,13 +1,10 @@
 elements.testing_element = {
     color: "#49a8ab",
     behavior: behavior,WALL,
-    category: "machine",
+    category: "testing",
     state: "solid",
     stateHigh: "carbon_dioxide",
     tempHigh: 100,
     templow:0,
     statelow:"ice"
 }
-
-
-
